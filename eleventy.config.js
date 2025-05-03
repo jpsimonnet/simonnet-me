2acd6ab1da1ce4f31a8c7e547f14fe63504c88ec
Jean-Philippe Simonnet <7069972+jpsimonnet@users.noreply.github.com>
2025-05-03 18:32:50 +0200

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,7 +5,7 @@ export default function(eleventyConfig) {
   import { HtmlBasePlugin } from "@11ty/eleventy";
 
   export const config = {
-    pathPrefix: "/simonnet-me",
+    pathPrefix: "/simonnet-me/",
     dir: {
       input: "src",
       output: "public",
