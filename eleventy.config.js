b640c3ef17d4471ff29d7118e92aca00f6aa28cd
Jean-Philippe Simonnet <7069972+jpsimonnet@users.noreply.github.com>
2025-05-03 13:30:04 +0200

    test

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,18 @@
 export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPlugin(HtmlBasePlugin);
   };
+  import { HtmlBasePlugin } from "@11ty/eleventy";
 
   export const config = {
     
     dir: {
       input: "src",
       output: "public",
-      includes: "modeles"
+      includes: "modeles",
+      pathPrefix: "/simonnet-me/",
     }
+
+   
   };
+
