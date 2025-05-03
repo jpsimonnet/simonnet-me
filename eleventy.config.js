4853c66a47a34aff517f043134b48efac4685697
Jean-Philippe Simonnet <7069972+jpsimonnet@users.noreply.github.com>
2025-05-03 17:50:20 +0200

    test aevc url

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,12 +5,11 @@ export default function(eleventyConfig) {
   import { HtmlBasePlugin } from "@11ty/eleventy";
 
   export const config = {
-    
+    pathPrefix: "/simonnet-me/",
     dir: {
       input: "src",
       output: "public",
       includes: "modeles",
-      pathPrefix: "/simonnet-me/",
     }
 
    
