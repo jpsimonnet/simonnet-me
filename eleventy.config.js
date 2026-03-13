52859bc2b71ade7bf7c6f75460b9f54c2e211e03
Jean-Philippe Simonnet <7069972+jpsimonnet@users.noreply.github.com>
2026-03-13 18:12:55 +0100

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,7 +15,7 @@ export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/assets/images");
-  eleventyConfig.addPassthroughCopy("src/rss/");
+  eleventyConfig.addPassthroughCopy("src/rss-facile");
   eleventyConfig.addPassthroughCopy({"src/assets/favicon": "/"});
   eleventyConfig.addPassthroughCopy({"src/assets/favicon": "./"});
   eleventyConfig.addPassthroughCopy("src/serment");
